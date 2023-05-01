7ddff0f66e66d3d29ff53703519d2d2c963aa1d9
Aslin Lesmani Cueva Martínez <113402130+Asliin@users.noreply.github.com>
2023-05-01 01:51:33 -0600

    Primeros avances
    Primeros avances del proyecto visto en clase

---
+++ Web/Modelos/Cliente.cs
@@ -0,0 +1,28 @@
+﻿namespace Modelos
+{
+    public class Cliente
+    {
+        public string Identidad { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Dirrecion { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool EstaActivo { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(string identidad, string nombre, string telefono, string correo, string dirrecion, DateTime fechaNacimiento, bool estaActivo)
+        {
+            Identidad = identidad;
+            Nombre = nombre;
+            Telefono = telefono;
+            Correo = correo;
+            Dirrecion = dirrecion;
+            FechaNacimiento = fechaNacimiento;
+            EstaActivo = estaActivo;
+        }
+    }
+}
